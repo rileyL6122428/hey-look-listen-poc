024f942e5571bc588c98298d468d68c12285d6d9
Riley Littlefield <rileylittlefield@ymail.com>
2019-04-11 01:30:35 -0400

    begins sample mobile client

--- spring-mvc-with-audio/src/main/java/com/example/demo/controller/AudioController.java
+++ spring-mvc-with-audio/src/main/java/com/example/demo/controller/AudioController.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AudioController {
 	
-	
+	@CrossOrigin
 	@GetMapping(path="/godzilla", produces="audio/mpeg")
 	@ResponseBody
 	public FileSystemResource getGodzillaAudio() {
